1fcc036505a2e306cfad28ad716767ad271b6b0d
syedfaizan4747 <syedfaizansfn@gmail.com>
2026-09-03 13:31:20 +0530

    Use environment secret key

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mmih4cekd8p(j@h+8s)_sim-d^5%dyw#qi(u09t63c53ujai7m'
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-development-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
